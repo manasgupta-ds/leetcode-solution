a7e139a44db56e42605b13d78dd457ee719783a5
manasgupta-ds <manasgupta.ds@gmail.com>
2026-09-09 00:06:55 +0530

    Time: 7 ms (8.21%), Space: 45 MB (70.49%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,8 +1,8 @@
-class Solution {
+class Solution 
+{
     public int[] nextGreaterElement(int[] nums1, int[] nums2) 
     {
         boolean found=false;
-        int b=0;
         boolean un=false;
         int arr[]=new int[nums1.length];
         for(int i=0;i<nums1.length;i++)
@@ -16,36 +16,25 @@ class Solution {
                     if(a==nums2[j])
                     {
                         un=true;
-                        b=a;
                     }
-                    if(b<nums2[j+1]&&un)
+                    if(a<nums2[j+1]&&un)
                     {
                     arr[i]=nums2[j+1];
                     found=true;
-                    System.out.println(arr[i]+"under");
                     break;
                     }
-                    }
-                    
-                }
-                
-                if(found==true)
-                {
-                    System.out.println("Manas");
-                    found=false;
-                    System.out.println(arr[i]+"foundf");
-
-                }
-                else
-                {
+                } 
+            }
+            if(found==true)
+            {
+                found=false;
+            }
+            else
+            {
                 arr[i]=-1;
-                System.out.println("Not found");
-                System.out.println(arr[i]+" NOt");
-                }
+            }
                 un=false;
         }
-            
-        
         return arr;
     }
 }
